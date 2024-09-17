506f6e7cb61883b13bba91c8b4a288c3c36bcaf9
Rogério <rogerioballoussier@icloud.com>
2024-09-17 16:56:24 -0300

    updated commit

---
+++ f8719981.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+dados = {
+    "REGRA1": ["N", "N", "S", "N"],
+    "REGRA2": ["S", "N", "S", "S"],
+    "REGRA3": ["N", "N", "S", "N"],
+    "REGRA4": ["N", "N", "S", "N"],
+    "REGRA5": ["S", "N", "S", "N"],
+}
+
+df = pd.DataFrame(dados)
+
+df["REGRAS"] = df.apply(lambda x: "".join(x[df.columns[-5:]]), axis=1)
+df["REGRAS"] = np.where("SSSSS" == df["REGRAS"], "S", "N")
+
+print(df)
